e586bd51c1cb4796a0975e65d8c6565d4f8bce9d
litc0de <litc0de@posteo.org>
2021-03-14 22:17:37 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d6642b8abeaa5c67f38e.js"
+    "url": "webpack-runtime-d8d58174b2b6a971e55f.js"
   },
   {
     "url": "framework-3e6dec6139dadaa7d0c7.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "725c125e5c4183ee7f46ffa26334699e"
+    "revision": "081207647e6994401c7daa46d3dcbf98"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-19245c8506e49b502b12.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "fe12915f3a15272602621033ddaaaffe"
+    "revision": "8c1d9f7bea3c0f285ab00870fd70149e"
   },
   {
     "url": "polyfill-38cce18603b1d20cac17.js"
